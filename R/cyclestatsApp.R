420789995c698fd57e15cf5d4403b418543a3b6e
esimms999 <esimms@yahoo.com>
2023-11-03 21:00:38 +0000

    Package01

---
+++ R/cyclestatsApp.R
@@ -0,0 +1,13 @@
+#' Cycle Stats app standalone function
+#'
+#' Wrapper function for `shinyApp()`
+#'
+#' @return shiny app
+#'
+#' @import shiny
+#'
+#' @export
+#'
+cyclestatsApp <- function() {
+  shinyApp(ui = cyclestats_ui, server = cyclestats_server)
+}
