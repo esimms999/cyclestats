6e010c5a1c0dbc3c7d345f40f7762ac4a3bf6342
esimms999 <esimms@yahoo.com>
2023-11-03 21:10:45 +0000

    Package02

---
+++ R/global.R
@@ -0,0 +1,38 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cyclestats_init <- function() {
+  cards <- list(
+    card(
+      full_screen = TRUE,
+      card_header("Bill Length"),
+      plotOutput("bill_length")
+    ),
+    card(
+      full_screen = TRUE,
+      card_header("Bill depth"),
+      plotOutput("bill_depth")
+    ),
+    card(
+      full_screen = TRUE,
+      card_header("Body Mass"),
+      plotOutput("body_mass")
+    )
+  )
+
+  color_by <- checkboxGroupInput(
+    inputId = "color_by",
+    label = "Year(s)",
+    choices = penguins[c("species", "island", "sex")],
+    selected = "species"
+  )
+
+  means <- colMeans(
+    penguins[c("bill_length_mm", "bill_length_mm", "body_mass_g")],
+    na.rm = TRUE
+  )
+}
