21e08c7b781e374d1883fd92ec1a89182a47400c
esimms999 <esimms@yahoo.com>
2023-11-11 00:33:33 +0000

    Added Martin code in attempt to avoid warning message about R/.

--- app.R
+++ app.R
@@ -8,8 +8,28 @@
 # This does not seem to work. Trying another proposed solution: R/_disable.R
 # options(shiny.autoload.r=FALSE)
 
-pkgload::load_all(export_all = FALSE,
-                  helpers = FALSE,
-                  attach_testthat = FALSE)
+# pkgload::load_all(export_all = FALSE,
+#                   helpers = FALSE,
+#                   attach_testthat = FALSE)
+#
+# cyclestats::cyclestatsApp()
+#
+# ++++++++++
 
-cyclestats::cyclestatsApp()
+  withr::with_options(new = list(shiny.autoload.r = FALSE), code = {
+      if (!interactive()) {
+      sink(stderr(), type = "output")
+      tryCatch(
+        expr = {
+          library(cyclestats)
+        },
+        error = function(e) {
+          pkgload::load_all()
+        }
+      )
+      } else {
+        pkgload::load_all()
+      }
+      cyclestats::cyclestatsApp()
+      # options = list(test.mode = TRUE), run = 'p')
+  })
