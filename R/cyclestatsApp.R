de04b47f64589b4ecad40682a67f66db4eefc6e2
esimms999 <esimms@yahoo.com>
2023-11-16 03:41:47 +0000

    Updated data as of 20231115. Modifies app.R based on Martin example. Removed attempt at option change in cyclestatsApp.R.

--- R/cyclestatsApp.R
+++ R/cyclestatsApp.R
@@ -9,6 +9,5 @@
 #' @export cyclestatsApp
 #'
 cyclestatsApp <- function() {
-  options(shiny.autoload.r=FALSE) # Attempt to avoid warning about source-ing files - not helping.
   shiny::shinyApp(ui = cyclestats_ui, server = cyclestats_server)
 }
--- app.R
+++ app.R
@@ -2,34 +2,24 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-#pkgload::load_all(".")
-
-# Set option to avoid warning about R directory, etc.
-# This does not seem to work. Trying another proposed solution: R/_disable.R
-# options(shiny.autoload.r=FALSE)
-
-# pkgload::load_all(export_all = FALSE,
-#                   helpers = FALSE,
-#                   attach_testthat = FALSE)
-#
-# cyclestats::cyclestatsApp()
-#
-# ++++++++++
-
   withr::with_options(new = list(shiny.autoload.r = FALSE), code = {
       if (!interactive()) {
       sink(stderr(), type = "output")
       tryCatch(
         expr = {
+          # load package
           library(cyclestats)
         },
         error = function(e) {
+          # load R/ folder
           pkgload::load_all()
         }
       )
       } else {
+        #load R/ folder
         pkgload::load_all()
       }
-      cyclestats::cyclestatsApp()
-      # options = list(test.mode = TRUE), run = 'p')
+
+    # create shiny object
+    cyclestats::cyclestatsApp()
   })
